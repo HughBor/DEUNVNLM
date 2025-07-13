97508e894a5d4640628e5831d0ff999c2a8cc4da
HughBor <1417844997@qq.com>
2025-07-13 22:24:15 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -21,7 +21,7 @@ const contentData = [
   {
     type: "image",
     title: "节日/产品海报展示",
-    contentUrl: ["images/PingMian/HaiBao/海报2011-黑五网一.jpg"],
+    contentUrl: ["images/PingMian/HaiBao/铰链_独立站二级页首横幅.jpg", "images/PingMian/HaiBao/海报2011-黑五网一.jpg"],
     thumbnail: "images/PingMian/HaiBao/海报2011-黑五网一封面.jpg",
     category: "平面",
     contentId: "Graphic 001",
