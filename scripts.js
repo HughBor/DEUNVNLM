f89f3254b8590fb813d52b6ccc94ac9e7e42fba2
HughBor <1417844997@qq.com>
2025-07-23 01:10:17 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ const contentData = [
   {
     type: "image",
     title: "修图-维纳斯铰链产品",
-    contentUrl: ["images/PingMian/XiuTu/Vvenace/铰链2in1.jpg", "images/PingMian/XiuTu/BT415主图.jpg"],
+    contentUrl: ["images/PingMian/XiuTu/Vvenace/铰链2in1.jpg", "images/PingMian/XiuTu/Vvenace/短臂铰链.jpg"],
     thumbnail: "images/PingMian/XiuTu/Vvenace/铰链2in1-封面.jpg",
     category: "平面",
     contentId: "Graphic 011",
